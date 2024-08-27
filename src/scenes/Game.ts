fe7903e34d46f34662af103d88b24bfa020d0dd8
Ancient237 <arthas.you@163.com>
2024-08-27 21:34:09 +0800

    fix: controller

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -1,45 +1,53 @@
-import { Scene } from 'phaser';
-import { GameController } from '../controllers/game_controller';
+import { Scene } from "phaser";
+import { GameController } from "../controllers/game_controller";
 
-export class Game extends Scene
-{
-    constructor ()
-    {
-        super('Game');
-    }
+export class Game extends Scene {
+  controller: GameController;
+  constructor() {
+    super("Game");
+  }
 
-    preload ()
-    {
-        this.load.setPath('assets');
-        this.load.image('star', 'star.png');
-        
-        this.load.image('background', 'bg.png');
-        this.load.image('logo', 'logo.png');
-    }
+  preload() {
+    this.load.setPath("assets");
+    this.load.image("star", "star.png");
 
-    create ()
-    {
-        this.controller = new GameController(this);
-        this.add.image(512, 384, 'background');
-        this.add.image(512, 350, 'logo').setDepth(100);
-        this.add.text(512, 490, 'Make something fun!\nand share it with us:\nsupport@phaser.io', {
-            fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
-            stroke: '#000000', strokeThickness: 8,
-            align: 'center'
-        }).setOrigin(0.5).setDepth(100);
-        
-        const button = this.add.image(512, 600, 'star')
-            .setInteractive() // 使按钮可交互
-            .setScale(0.5); // 设置按钮的大小比例
+    this.load.image("background", "bg.png");
+    this.load.image("logo", "logo.png");
+  }
 
-        // 按钮点击事件
-        button.on('pointerdown', () => this.handleButtonClick());
+  create() {
+    this.controller = new GameController(this);
+    this.add.image(512, 384, "background");
+    this.add.image(512, 350, "logo").setDepth(100);
+    this.add
+      .text(
+        512,
+        490,
+        "Make something fun!\nand share it with us:\nsupport@phaser.io",
+        {
+          fontFamily: "Arial Black",
+          fontSize: 38,
+          color: "#ffffff",
+          stroke: "#000000",
+          strokeThickness: 8,
+          align: "center",
+        }
+      )
+      .setOrigin(0.5)
+      .setDepth(100);
 
-    }
+    const button = this.add
+      .image(512, 600, "star")
+      .setInteractive() // 使按钮可交互
+      .setScale(0.5); // 设置按钮的大小比例
 
-    private handleButtonClick() {
-        // 在这里处理按钮点击事件，例如跳转到下一个场景或执行其他操作
-        this.controller.send_cmd();
-        console.log('Handle the button click event here.');
-    }
+    // 按钮点击事件
+    button.on("pointerdown", () => this.handleButtonClick());
+  }
+
+  private handleButtonClick() {
+    // 在这里处理按钮点击事件，例如跳转到下一个场景或执行其他操作
+    this.controller.send_cmd();
+    console.log("Handle the button click event here.");
+  }
 }
